f8ccbae61571e46d9525ad89b1929b902b209a57
Russell Damerell-Moss <russell.damerell@me.com>
2024-11-11 11:55:34 +0200

    Initial

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,24 @@
+return {
+  "aktersnurra/no-clown-fiesta.nvim",
+  "datsfilipe/vesper.nvim",
+  "projekt0n/github-nvim-theme",
+  "dustypomerleau/tol.nvim",
+  "drazil100/dusklight.vim",
+  "slugbyte/lackluster.nvim",
+  "olivercederborg/poimandres.nvim",
+  "paulfrische/reddish.nvim",
+  "nyoom-engineering/oxocarbon.nvim",
+  {
+    "wurli/cobalt.nvim",
+    opts = {
+      overrides = function(colors)
+        return {
+          LspReferenceWrite = { bg = colors.palette.MintyFresh, fg = colors.palette.MochaBrown },
+          LspReferenceRead = { bg = colors.palette.MintyFresh, fg = colors.palette.MochaBrown },
+          LspReferenceText = { bg = colors.palette.MintyFresh, fg = colors.palette.MochaBrown },
+          vim_lsp_references = { bg = colors.palette.MintyFresh, fg = colors.palette.MochaBrown },
+        }
+      end,
+    },
+  },
+}
